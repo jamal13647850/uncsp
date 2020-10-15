c486b9c05bcf9a9d88d184f3bbcc6ed233ac4a3d
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2020-10-15 19:23:55 +0330

    First commit

---
+++ 04/assets/js/scripts.js
@@ -0,0 +1,4 @@
+function testFunction(){
+  alert("Hello . Welcome to my fourth parctice. This exercise is about external functions");
+  document.write("I write the alert and this text in function and then i call this function. </br> I definded function in a file and then add my scripts file to html page");
+}
